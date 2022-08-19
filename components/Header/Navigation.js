898808681b513a3df0cb3da7f3d9ab62ecb31b99
HAMZA NAWAB <hamzanawab.bs@gmail.com>
2022-08-20 01:07:40 +0500

    Navigation bar almost done

--- components/Header/Navigation.js
+++ components/Header/Navigation.js
@@ -1,17 +1,33 @@
 const Navigation = () => {
     return (
-        <div className="flex items-center justify-between h-40">
+        <div className="flex items-center justify-between h-24 px-10">
             <div>
-                <h1>VacationRental</h1>
+                <a href="#"><h1 className="text-2xl font-extrabold">Vacation<span className="text-color-primary">Rental</span></h1></a>
             </div>
-            <nav>
-                <ul className="flex">
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Services</li>
-                    <li>Apartment Room</li>
-                    <li>Blog</li>
-                    <li>Contact</li>
+            <nav className="lg:h-full cursor-pointer">
+                <div className="lg:hidden flex items-center justify-center">
+                    <svg class="w-6 h-6 lg:hidden text-[#000]/20" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+                    <h2 className="text-[#000]/20 text-lg lg:hidden block">MENU</h2>
+                </div>
+                <ul className="lg:flex text-[#000]/70 h-full items-center hidden">
+                    <div className="lg:h-full lg:flex items-center lg:justify-center cursor-pointer lg:w-24 hover:text-color-primary transition-all ease-out duration-500 lg:nav_item">
+                        <li>Home</li>
+                    </div>
+                    <div className="lg:h-full lg:flex items-center lg:justify-center cursor-pointer lg:w-24 hover:text-color-primary transition-all ease-out duration-500 lg:nav_item">
+                        <li>About</li>
+                    </div>
+                    <div className="lg:h-full lg:flex items-center lg:justify-center cursor-pointer lg:w-24 hover:text-color-primary transition-all ease-out duration-500 lg:nav_item">
+                        <li>Services</li>
+                    </div>
+                    <div className="lg:h-full lg:flex items-center lg:justify-center cursor-pointer lg:w-40 hover:text-color-primary transition-all ease-out duration-500 lg:nav_item">
+                        <li>Apartment Room</li>
+                    </div>
+                    <div className="lg:h-full lg:flex items-center lg:justify-center cursor-pointer lg:w-24 hover:text-color-primary transition-all ease-out duration-500 lg:nav_item">
+                        <li>Blog</li>
+                    </div>
+                    <div className="lg:h-full lg:flex items-center lg:justify-center cursor-pointer lg:w-24 hover:text-color-primary transition-all ease-out duration-500 lg:nav_item">
+                        <li>Contact</li>
+                    </div>
                 </ul>
             </nav>
         </div>
